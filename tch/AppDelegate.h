85f45ac5faae2ebcec57d39fd282d20d30f88da2
mengyun <mengyun@mengyundeMacBook-Pro.local>
2015-12-11 21:49:15 +0800

    init

---
+++ tch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tch
+//
+//  Created by mengyun on 15/10/3.
+//  Copyright © 2015年 mengyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tch
+//
+//  Created by mengyun on 15/10/3.
+//  Copyright © 2015年 mengyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
